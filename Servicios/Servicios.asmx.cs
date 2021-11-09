87c4daae3db5ca2478a6b58244269c68309c71d6
Luis Enrique <enrique_inf@hotmail.com>
2021-11-09 17:47:54 -0600

    primer commit

---
+++ Servicios/Servicios.asmx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Servicios
+{
+    /// <summary>
+    /// Descripción breve de Servicios
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Servicios : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+
+        [WebMethod(Description = "Saluda a una persona")]
+        public string Saludar(string nombre)
+        {
+            return $"Hola {nombre}";
+        }
+
+        [WebMethod(Description ="Log")]
+        public string GuardarLog(string mensaje) 
+        {
+            funciones.Logs("LogServicio", mensaje);
+            return "Se escribio un evento en el log";
+        }
+    }
+}
---
+++ Servicios/system/funciones.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Servicios
+{
+    public class funciones
+    {
+        public static void Logs(string nombre_archivo, string descripcion) 
+        {
+            //Creamos la estructura de directorios por año, mes, dia
+            string directorio = AppDomain.CurrentDomain.BaseDirectory + "logs/" +
+                DateTime.Now.Year.ToString() + "/" +
+                DateTime.Now.Month.ToString() + "/" +
+                DateTime.Now.Day.ToString();
+
+            //verificamos si existe el direcotorio
+            //En caso de no existir, entra al if y se ejecuta el metodo para crearlo
+            if (!Directory.Exists(directorio))
+            {
+                Directory.CreateDirectory(directorio);
+            }
+
+            StreamWriter mi_archivo = new StreamWriter(directorio+"/"+nombre_archivo+".log",true);
+
+            string cadena = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")+ ">>>"+descripcion;
+
+            mi_archivo.WriteLine(cadena);
+            mi_archivo.Close();
+        }
+    }
+}
